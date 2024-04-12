31bb10c9554822f8c4cbe9182dcef14734770fd8
hoseong511 <thdghtjd115@naver.com>
2024-04-12 17:30:30 +0900

    feat header today

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 import modalEvent from "./modal.js";
+import getheaderToday from "./header.js";
 import { dashboardEvent } from "./dashboard.js";
 
 modalEvent();
 dashboardEvent();
+getheaderToday();
