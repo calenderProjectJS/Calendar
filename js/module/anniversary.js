dcb8dde3fb52f31c58d743b2a8dccd5afe9fa17a
hoseong511 <thdghtjd115@naver.com>
2024-04-16 22:56:58 +0900

    feat holiday 2020 ~ 2030

--- js/module/anniversary.js
+++ js/module/anniversary.js
@@ -1,4 +1,9 @@
-import { today } from "../utils.js";
+/* 
+	- 서버로 이전 될 부분
+	- 서버 시작 시, db에 저장 (2020 ~ 2030년)
+	- 이후 요청 연도와 월에 관해서 추가 api 요청 기능
+*/
+
 import { saveHolidayList, loadHolidayList } from "../data/localStorage.js";
 
 const xhr = new XMLHttpRequest();
@@ -32,27 +37,29 @@ const queryRest = (key, obj) => {
 	return queryParams;
 }
 
-const requestXML = () => {
-	for (let index = 0; index < 12; index++) {
-		xhr.open('GET', url + get.restDel + queryAnni(key, {year: today.year, month: index}), false);
-		xhr.onreadystatechange = function async () {
-			const dates = loadHolidayList();
-			if (this.readyState === 4 && this.status === 200) {
-				const parser = new DOMParser();
-				const xmlDoc = parser.parseFromString(this.responseText, "text/xml");
-				const $items = xmlDoc.querySelectorAll("body item");
-				for (const iter of [...$items]) {
-					dates.push({
-						name: iter.getElementsByTagName("dateName")[0].textContent,
-						locdate: iter.getElementsByTagName("locdate")[0].textContent,
-						isHoliday: iter.getElementsByTagName("isHoliday")[0].textContent,
-					});
+const initHolidayData = () => {
+	for (let year = 2020; year < 2031; year++) {
+		for (let month = 0; month < 12; month++) {
+			xhr.open('GET', url + get.restDel + queryAnni(key, {year, month}), false);
+			xhr.onreadystatechange = function async () {
+				const dates = loadHolidayList();
+				if (this.readyState === 4 && this.status === 200) {
+					const parser = new DOMParser();
+					const xmlDoc = parser.parseFromString(this.responseText, "text/xml");
+					const $items = xmlDoc.querySelectorAll("body item");
+					for (const iter of [...$items]) {
+						dates.push({
+							name: iter.getElementsByTagName("dateName")[0].textContent,
+							locdate: iter.getElementsByTagName("locdate")[0].textContent,
+							isHoliday: iter.getElementsByTagName("isHoliday")[0].textContent,
+						});
+					}
+					saveHolidayList(dates);
 				}
-				saveHolidayList(dates);
-			}
-		};
-		xhr.send('');
+			};
+			xhr.send('');
+		}
 	}
 }
-if (loadHolidayList().length === 0) requestXML();
+if (loadHolidayList().length === 0) initHolidayData();
 export default xhr;
