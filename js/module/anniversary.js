641a0aaa7ad26d7b4b21c457f9988e3605914259
hoseong511 <thdghtjd115@naver.com>
2024-04-16 23:41:26 +0900

    refact request func
    - xmlhttprequest -> request()
    - callback

--- js/module/anniversary.js
+++ js/module/anniversary.js
@@ -5,6 +5,7 @@
 */
 
 import { saveHolidayList, loadHolidayList } from "../data/localStorage.js";
+import { getDateInfoFromText, getDateInfoFromText2 } from "../utils.js";
 
 const xhr = new XMLHttpRequest();
 const key = 'jnBzwmpZT2R23RcIULm5xeKznct7Zm4TJKgGluZ2AwLSTnBDypkC57KykTZ599myibI9pU%2FUf2UQ%2FIv1Ke2cYA%3D%3D';
@@ -37,29 +38,53 @@ const queryRest = (key, obj) => {
 	return queryParams;
 }
 
+/**
+ 	@param callback: func(xmlDoc)
+	@param key: api encode key
+	@param year: number
+	@param month: number
+ *  */ 
+const request = (callback, key, year, month) => {
+	xhr.open('GET', url + get.restDel + queryAnni(key, { year, month }), false);
+	xhr.onreadystatechange = function async() {
+		if (this.readyState === 4 && this.status === 200) {
+			const parser = new DOMParser();
+			const xmlDoc = parser.parseFromString(this.responseText, "text/xml");
+			callback(xmlDoc);
+		}
+	};
+	xhr.send('');
+}
+
 const initHolidayData = () => {
 	for (let year = 2020; year < 2031; year++) {
 		for (let month = 0; month < 12; month++) {
-			xhr.open('GET', url + get.restDel + queryAnni(key, {year, month}), false);
-			xhr.onreadystatechange = function async () {
-				const dates = loadHolidayList();
-				if (this.readyState === 4 && this.status === 200) {
-					const parser = new DOMParser();
-					const xmlDoc = parser.parseFromString(this.responseText, "text/xml");
-					const $items = xmlDoc.querySelectorAll("body item");
-					for (const iter of [...$items]) {
-						dates.push({
-							name: iter.getElementsByTagName("dateName")[0].textContent,
-							locdate: iter.getElementsByTagName("locdate")[0].textContent,
-							isHoliday: iter.getElementsByTagName("isHoliday")[0].textContent,
-						});
-					}
-					saveHolidayList(dates);
+			request((xmlDoc) => {
+				const $items = xmlDoc.querySelectorAll("body item");
+				for (const iter of [...$items]) {
+					dates.push({
+						name: iter.getElementsByTagName("dateName")[0].textContent,
+						locdate: iter.getElementsByTagName("locdate")[0].textContent,
+						isHoliday: iter.getElementsByTagName("isHoliday")[0].textContent,
+					});
 				}
-			};
-			xhr.send('');
+				saveHolidayList(dates);
+			}, key, year, month);
 		}
 	}
 }
 if (loadHolidayList().length === 0) initHolidayData();
-export default xhr;
+
+const test = () => {
+	const holidayList = loadHolidayList();
+	const list = holidayList.map(element => {
+		return {
+			title: element.name,
+			time: getDateInfoFromText2(element.locdate),
+		}
+	});
+	list.forEach(element => {
+		console.log(element);
+	});
+}
+export { xhr, test };
